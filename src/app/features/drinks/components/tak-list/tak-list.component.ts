7b6413d7c979aeaf8b77b748a9460279a8686e1c
Thomas de Wulf <thomas.de.wulf@telenet.be>
2020-01-10 17:32:13 +0100

    Conceptualising layout

---
+++ src/app/features/drinks/components/tak-list/tak-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tak-list',
+  templateUrl: './tak-list.component.html',
+  styleUrls: ['./tak-list.component.scss']
+})
+export class TakListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/features/drinks/drinks.module.ts
+++ src/app/features/drinks/drinks.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DrinksListComponent } from './pages/drinks-list/drinks-list.component';
 import { DrinksRoutingModule } from './drinks-routing.module';
+import { TakListComponent } from './components/tak-list/tak-list.component';
+import { MaterialModule } from 'src/app/core/material/material.module';
 
 
 
 @NgModule({
-  declarations: [DrinksListComponent],
+  declarations: [DrinksListComponent, TakListComponent],
   imports: [
     CommonModule,
-    DrinksRoutingModule
+    DrinksRoutingModule,
+    MaterialModule
   ]
 })
 export class DrinksModule { }
