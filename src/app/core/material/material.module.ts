d0348ccbd9a90e5b3ed04138ff95fb9d6a58ad98
Thomas de Wulf <thomas.de.wulf@telenet.be>
2020-01-11 21:46:58 +0100

    Add effects to make card appear as a button

--- src/app/core/material/material.module.ts
+++ src/app/core/material/material.module.ts
@@ -8,6 +8,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatCardModule } from '@angular/material/card';
+import { MatRippleModule } from '@angular/material/core';
 
 
 @NgModule({
@@ -32,6 +33,7 @@ import { MatCardModule } from '@angular/material/card';
     MatListModule,
     MatSlideToggleModule,
     MatCardModule,
+    MatRippleModule
   ]
 })
 export class MaterialModule {}
