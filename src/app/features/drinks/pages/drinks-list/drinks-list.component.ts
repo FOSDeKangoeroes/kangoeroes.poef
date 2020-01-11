990f3f0af7112007b90469deb4154ffdf17ec23b
Thomas de Wulf <thomas.de.wulf@telenet.be>
2020-01-11 11:00:37 +0100

    Start work on mobile layout - WIP

--- src/app/features/drinks/pages/drinks-list/drinks-list.component.ts
+++ src/app/features/drinks/pages/drinks-list/drinks-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-drinks-list',
@@ -6,11 +9,15 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./drinks-list.component.scss']
 })
 export class DrinksListComponent implements OnInit {
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
 
+  constructor(private breakpointObserver: BreakpointObserver) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
