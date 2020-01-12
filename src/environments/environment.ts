bb7d525fbe5716e8561ddbc0bde395e3e5a7c8c2
Thomas de Wulf <thomas.de.wulf@telenet.be>
2020-01-12 11:06:06 +0100

    I'm bad at urls

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  appUrl: 'http://staging-kangoeroe-api.azurewebsites.net/api'
+  appUrl: 'https://staging-kangoeroe-api.azurewebsites.net/api'
 };
 
 /*
