08eefb2347d6ad3833910f7a2358e15ac0d26a46
Thomas de Wulf <thomas.de.wulf@telenet.be>
2020-01-18 15:55:58 +0100

    Clean unused imports

--- src/app/features/drinks/pages/drinks-list/drinks-list.component.ts
+++ src/app/features/drinks/pages/drinks-list/drinks-list.component.ts
@@ -9,11 +9,9 @@ import {
   AfterViewInit
 } from '@angular/core';
 
-import { MatSidenav } from '@angular/material/sidenav';
 import { DeviceDetectionService } from 'src/app/core/device-detection/device-detection.service';
 import { DrinksNavComponent } from '../../components/drinks-nav/drinks-nav.component';
 import { DrinksDataService } from '../../shared/drinks-data.service';
-import { Observable } from 'rxjs';
 import { Tak } from '../../shared/tak.model';
 import { Leiding } from '../../shared/leiding.model';
 import { Drank } from '../../shared/drank.model';
